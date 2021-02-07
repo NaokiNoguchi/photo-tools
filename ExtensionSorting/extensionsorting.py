15868ad7b03873bcf070e914763492df30a19b96
NaokiNoguchi <krunaru223@gmail.com>
2021-02-07 20:31:59 +0900

    [update]Added the process to open the configuration file

--- ExtensionSorting/extensionsorting.py
+++ ExtensionSorting/extensionsorting.py
@@ -20,17 +20,22 @@ def check_directorypath(sorce_path):
     return result
 
 # 拡張子の種類でファイルに仕分けを行う
-def classify(sorce_path ,json_dict):
+def classify(sorce_path):
+    # configファイルを開く
+    json_load = None
+    with open("config.json","r") as json_file:
+        json_load = json.load(json_file)
+
     # 入力ディレクトリのファイルをループで処理する
     for file in os.listdir(sorce_path):
         base, ext = os.path.splitext(file)
         # 拡張子の種類ごとに仕分けを行う
-        for key in json_dict:
+        for key in json_load:
             # 移動先のディレクトリ名
             directory_name = sorce_path + "/" + key + "/"
             os.makedirs(directory_name, exist_ok=True)
             # 類似した拡張子を調べる
-            for value in json_dict[key]:
+            for value in json_load[key]:
                 # valueと同じ拡張子のファイルがあれば移動する
                 if ext == "."+value:
                     shutil.move(sorce_path+"/"+file, directory_name)
