ddea7b247c30494752b99b77e67159dc4f5c6d76
NaokiNoguchi <krunaru223@gmail.com>
2021-02-07 20:20:57 +0900

    [add]Added a funciton to sort by extension

--- ExtensionSorting/extensionsorting.py
+++ ExtensionSorting/extensionsorting.py
@@ -19,5 +19,21 @@ def check_directorypath(sorce_path):
         result = False
     return result
 
+# 拡張子の種類でファイルに仕分けを行う
+def classify(sorce_path ,json_dict):
+    # 入力ディレクトリのファイルをループで処理する
+    for file in os.listdir(sorce_path):
+        base, ext = os.path.splitext(file)
+        # 拡張子の種類ごとに仕分けを行う
+        for key in json_dict:
+            # 移動先のディレクトリ名
+            directory_name = sorce_path + "/" + key + "/"
+            os.makedirs(directory_name, exist_ok=True)
+            # 類似した拡張子を調べる
+            for value in json_dict[key]:
+                # valueと同じ拡張子のファイルがあれば移動する
+                if ext == "."+value:
+                    shutil.move(sorce_path+"/"+file, directory_name)
+
 # if __name__ == '__main__':
 
